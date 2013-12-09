f1449fc3b90d96961d7f021f88bc1134af26e142
Jorge Ornelas <jorge@ubuntu.(none)>
2013-12-08 22:15:12 -0800

    Finished functions that read and write to encrypted files

--- enc_rw.py
+++ enc_rw.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python              
+
 from Crypto.Cipher import AES
+import os
 
 def read_enc(key, ciphname):
 
-    obj = AES.new(key, AES.MODE_CBC)
+    obj = AES.new(key, AES.MODE_ECB)
 
     f = open(ciphname, "r")
     c = f.read()
@@ -12,15 +15,53 @@ def read_enc(key, ciphname):
 
 def write_enc(key, ciphname, text):
 
-    obj = AES.new(key, AES.MODE_CBC)
+    obj = AES.new(key, AES.MODE_ECB)
 
-    f = open(ciphname, "w")
+    f = open(ciphname, "r+")
 
+    # create new temp file
     newf =  open("temp"+ciphname, "w")
-
-    plain = obj.decrypt(f.read())
-
+    newf.close()
+    newf =  open("temp"+ciphname, "r+")
+    
+    # copy file content to temp file 
+    cipher = f.read()
+    f.close()
+    plain = str(obj.decrypt(cipher))
     newf.write(plain)
-    newf.write(text)
 
-    c = obj.encrypt(newf.read)
+    # add new content to temp file
+    newf.write(text)
+    newf.close()
+
+    # encrypt old and new content
+    newf =  open("temp"+ciphname, "r+")    
+    ciph = newf.read()
+    c = obj.encrypt(ciph)
+
+    # write new encrypted content to file
+    with open(ciphname, "w") as f:
+        f.write(c)
+    f.close()
+    newf.close()
+    
+    #delete temp file
+    os.remove("temp"+ciphname)
+    
+
+obj = AES.new("asdfasdfasdfasdf", AES.MODE_ECB) 
+fileA = open("test", "w")
+
+secret = "Secret text that should be encry"
+cipher = obj.encrypt(secret)
+
+fileA.write(cipher)
+fileA.close()
+
+print read_enc("asdfasdfasdfasdf", "test")
+
+write_enc("asdfasdfasdfasdf", "test", "the cat jumped over the hot roof")
+
+print read_enc("asdfasdfasdfasdf", "test")
+
+# if you cat the file, you will see that it is still encrypted
