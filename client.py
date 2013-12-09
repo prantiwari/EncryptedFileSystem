530bdecfa441654fe5575715baa8c5e524a85248
Ulziibayar Otgonbaatar <j2i2t2u2@gmail.com>
2013-12-09 15:18:59 -0500

    Added minimally functional server and client

--- client.py
+++ client.py
@@ -1,1 +1,71 @@
-// client. there can be multiple, but start off with one
+#!/usr/bin/env python
+# Initial client version.
+import httplib, urllib, getopt
+import argparse
+
+def get_handler(arg):
+    conn = httplib.HTTPConnection("localhost:8000")
+    conn.request("GET", "/"+arg.name)
+    r1 = conn.getresponse()
+    print r1.status, r1.reason
+    data1 = r1.read()
+    print "Data in the file is: "+ data1
+    conn.close()
+    
+def put_handler(arg):
+    params = urllib.urlencode({'data': arg.data})
+    headers = {"Content-type": "application/x-www-form-urlencoded",
+    "Accept": "text/plain"}
+    conn = httplib.HTTPConnection("localhost:8000")
+    conn.request("POST", "/"+ arg.name, params, headers)
+    response = conn.getresponse()
+    print response.status, response.reason
+    data = response.read()
+    conn.close()
+def usage():
+    print ' -------------------------------------------------------------------------'
+    print ' MIT 6.854 Final project, 2013'
+    print ' Team: ulziibay, joor2992,  otitochi @ mit.edu'
+    print ' '
+    print ' This is EncrypFS file system'
+    print ' '
+    print ' Usage:'
+    print ' client.py --h'
+    print ' client.py --g my_name.txt'
+    print ' client.py --p my_name.txt "THIS IS A TEST DATA"'
+    print ' '
+    print ' --g Get the file with the name'
+    print ' --p  Put the file and data'
+    print ' -------------------------------------------------------------------------'
+    sys.exit(' ')
+
+    
+def main():
+
+    parser = argparse.ArgumentParser(description='Run EncrypFS client')
+    subparsers = parser.add_subparsers(help='Program mode (e.g. Put or Get a file, etc.)', dest='mode')
+    get_parser = subparsers.add_parser('get', help='Choose whether to get or put a file')
+    get_parser.add_argument('-n',
+                            '--name',
+                            required=True,
+                            help='Get a file with name')
+    put_parser = subparsers.add_parser('put', help='Put a file')
+    put_parser.add_argument('-n',
+                            '--name',
+                            required=True,
+                            help='Put a file with name')
+    put_parser.add_argument('-d',
+                            '--data',
+                            required=True,
+                            help='Specify file content')
+    args = parser.parse_args()
+    print '\n\n'
+    if args.mode == "get":
+        get_handler(args)
+    elif args.mode == "put":
+        put_handler(args)
+    else:
+        usage()
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,1 +1,66 @@
-// server
+#Initial version of the server
+import sys
+import time
+import BaseHTTPServer
+import os
+from os import curdir
+from os.path import join as pjoin
+
+HOST_NAME = '127.0.0.1' #TODO change this eventually to amazon host or similar.
+PORT_NUMBER = 8000 # Maybe set this to 8080.
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("<html><head><title>Title goes here.</title></head>")
+        s.wfile.write("<body><p>This is a test.</p>")
+        s.wfile.write("<p>You accessed path: %s</p>" % s.path)
+        file_name = s.path[s.path.rfind("/") + 1:]
+        abs_path = os.path.abspath(pjoin(curdir, file_name))
+        if os.path.exists(abs_path):
+            s.wfile.write("<p>File exists: %s</p>" % pjoin(curdir, s.path))
+            content = ""
+            with open(abs_path, 'r') as fh:
+                content =  fh.read()  
+            s.wfile.write("<p>File content: %s</p>" % content)
+        s.wfile.write("</body></html>")
+    def do_POST(s):
+        """Respond to a POST request."""
+        if s.path.endswith(".txt"):
+            file_name = s.path[s.path.rfind("/") + 1:]
+            store_path = pjoin(curdir, file_name)
+            
+            s.wfile.write('Client: %s\n' % str(s.client_address))
+            s.wfile.write('Path: %s\n' % s.path)
+            length = s.headers['content-length']
+            data = s.rfile.read(int(length))
+
+            with open(store_path, 'w') as fh:
+                fh.write(data.decode())
+            
+            s.send_response(200)
+            s.send_header("Content-type", "text/html")
+            s.end_headers()
+        else:
+            # Accept txt file for now. 
+            # TODO support rich file format
+            s.send_response(304)
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+
